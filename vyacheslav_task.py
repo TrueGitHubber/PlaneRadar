bb8f47eeaaae95f50990bde9b5af9e6ebc25e4d9
Alexander <AntipinBFU@yandex.ru>
2019-06-18 16:45:04 +0200

    deleted unnecessary files

--- vyacheslav_task.py
+++
@@ -1,9 +0,0 @@
-
-import folium
- 
-map = folium.Map(location=[54.890049,20.59263], zoom_start = 8, tiles= "CartoDB dark_matter")
- 
-for coordinates in [[54.890049,20.59263]]:
-    folium.Marker(location=coordinates, icon=folium.Icon(color = 'orange')).add_to(map)
- 
-map.save("map1.html")
