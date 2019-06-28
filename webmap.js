290d4aeffee7a7dd450bf2f52d8898bb60f0c7c7
Alexander <AntipinBFU@yandex.ru>
2019-06-28 13:02:27 +0200

    Added max size of zoom and clone object on map

--- webmap.js
+++ webmap.js
@@ -5,7 +5,9 @@ var webmap = L.map
 				center: [54.89, 20.59],
 				crs: L.CRS.EPSG3857,
 				zoom: 5,
+				minZoom: 3,
 				zoomControl: true,
 				preferCanvas: false,
+				worldCopyJump: true,
 			}
 		);
